e948186b139d18af0efce8c61ee476c4704e1e96
e1n22012 <e1n22012@st.oit.ac.jp>
2024-10-11 21:11:25 +0900

    FowardPositionの変更

---
+++ Assets/Scripts/FowardPosition/Mondai_F2.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mondai_F2 : MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject mondaiObject;
+    public GameObject seikaiObject;
+    public GameObject fuseikaiObject;
+    public GameObject cancelObject;
+    public GameObject arrowObject;
+    public GameObject inventoryObject;
+    public GameObject clockObject;
+    public GameObject canopen_clockObject;
+    public float hideDelay = 2.0f; // 非表示にするまでの時間（秒）
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = inputField.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputText()
+    {
+        if (inputField.text == "きみつ、かよう、かんぜん" || inputField.text == "きみつ,かよう,かんぜん")
+        {
+            seikaiObject.SetActive(true);
+            cancelObject.SetActive(true);
+            mondaiObject.SetActive(false);
+            arrowObject.SetActive(true);
+            inventoryObject.SetActive(true);
+            clockObject.SetActive(true);
+            canopen_clockObject.SetActive(false);
+
+        }
+        else
+        {
+            fuseikaiObject.SetActive(true);
+            cancelObject.SetActive(true);
+        }
+
+    }
+
+    /*IEnumerator HideSeikaiObjectAfterDelay()
+    {
+        yield return new WaitForSeconds(hideDelay); // 指定した秒数待機
+        seikaiObject.SetActive(false); // 非表示にする
+    }*/
+}
