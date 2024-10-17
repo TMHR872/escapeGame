fa01478b32fc7ad8ed5a74ba53e10d0f6ffcd540
e1n22012 <e1n22012@st.oit.ac.jp>
2024-10-17 23:34:55 +0900

    犬猫の追加

---
+++ Assets/Scripts/RightPosition/Mondai_R1.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mondai_R1 : MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject mondaiObject;
+    public GameObject seikaiObject;
+    public GameObject fuseikaiObject;
+    public GameObject cancelObject;
+    public GameObject arrowObject;
+    public GameObject inventoryObject;
+    public GameObject hikoukiObject;
+    public GameObject canopen_hikoukiObject;
+    public float hideDelay = 2.0f; // 非表示にするまでの時間（秒）
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = inputField.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputText()
+    {
+        if (inputField.text == "3")
+        {
+            cancelObject.SetActive(true);
+            mondaiObject.SetActive(false);
+            arrowObject.SetActive(true);
+            inventoryObject.SetActive(true);
+            hikoukiObject.SetActive(true);
+            canopen_hikoukiObject.SetActive(false);
+            seikaiObject.SetActive(true);
+
+        }
+        else
+        {
+            fuseikaiObject.SetActive(true);
+            cancelObject.SetActive(true);
+        }
+
+    }
+
+    /*IEnumerator HideSeikaiObjectAfterDelay()
+    {
+        yield return new WaitForSeconds(hideDelay); // 指定した秒数待機
+        seikaiObject.SetActive(false); // 非表示にする
+    }*/
+}
