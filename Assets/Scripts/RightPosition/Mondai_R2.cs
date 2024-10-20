c55d214f484556d2de41233955f3f42f903e59d8
e1n22012 <e1n22012@st.oit.ac.jp>
2024-10-20 16:16:52 +0900

    タイトル画面に戻るなどの修正

---
+++ Assets/Scripts/RightPosition/Mondai_R2.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mondai_R2 : MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject mondaiObject;
+    public GameObject seikaiObject;
+    public GameObject fuseikaiObject;
+    public GameObject cancelObject;
+    public GameObject arrowObject;
+    public GameObject inventoryObject;
+    public GameObject artObject;
+    public GameObject canOpen_artObject;
+    public float hideDelay = 2.0f; // 非表示にするまでの時間（秒）
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = inputField.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputText()
+    {
+        if (inputField.text == "hirakata" || inputField.text == "HIRAKATA")
+        {
+            seikaiObject.SetActive(true);
+            cancelObject.SetActive(true);
+            mondaiObject.SetActive(false);
+            arrowObject.SetActive(true);
+            inventoryObject.SetActive(true);
+            artObject.SetActive(true);
+            canOpen_artObject.SetActive(false);
+        }
+        else
+        {
+            fuseikaiObject.SetActive(true);
+            cancelObject?.SetActive(true);
+        }
+
+    }
+
+    /*IEnumerator HideSeikaiObjectAfterDelay()
+    {
+        yield return new WaitForSeconds(hideDelay); // 指定した秒数待機
+        seikaiObject.SetActive(false); // 非表示にする
+    }*/
+}
